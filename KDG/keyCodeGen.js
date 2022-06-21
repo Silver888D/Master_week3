dbf8a6b62fef29a787ef9e09f5066f843d2e6c31
Seb Amos <sebastian.amos8@gmail.com>
2022-06-21 15:51:50 +0100

    fewchagnes

--- KDG/keyCodeGen.js
+++ KDG/keyCodeGen.js
@@ -2,11 +2,12 @@ window.setTimeout((welcome)=>{
     alert(welcome);},150,"Welcome to the keycode Generator");
 
 const displayKey = document.querySelector(".key h2");
-const displayKeyCode = document.querySelector(".keyCode h2");
 const displayKeyClass = document.querySelector(".keyClass h2");
+const displayKeyCode = document.querySelector(".keyCode h2");
+
 
 window.addEventListener("keydown",(e)=>{
     displayKey.innerText = e.key;
-    displayKeyCode.innerText = e.keyCode;
-    displayKeyClass.innerText = e.code;        
+    displayKeyClass.innerText = e.code;
+    displayKeyCode.innerText = e.keyCode;      
     if(e.keyCode === 32){displayKey.innerText ="Space-bar";}});
